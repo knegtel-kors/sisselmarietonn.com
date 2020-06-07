acae1002ccdaeadd4b1f1a2392f3a85dfeae1e3b
Titus Knegtel <titusknegtel@Tituss-MacBook-Pro.local>
2020-06-08 00:40:40 +0200

    Initial styling small projects

--- components/SmallProjectList.vue
+++ components/SmallProjectList.vue
@@ -1,23 +1,38 @@
 <template>
-  <div class="small-projects">
-    <div class="small-project" v-for="project in projects" :key="project.id">
-
-      <nuxt-link class="project" :to="project.slugs[0]">
-        <prismic-rich-text :field="project.data.title" />
-      </nuxt-link>
-
-      <FormattedImage :field="project.data.header" :width="200" :height="400" />
-
-      <div class="type">
-        {{ project.data.type }}
+  <div class="project-wrapper small">
+    <div class="block-project" v-for="project in projects" :key="project.id">
+      <!-- Image -->
+      <FormattedImage class="project-img" :field="project.data.header" :width="400" :height="300" />
+      <div class="project-info">
+        <!-- Title -->
+        <nuxt-link class="project" :to="project.slugs[0]">
+          <prismic-rich-text :field="project.data.title" />
+        </nuxt-link>
+        <div class="project-data">
+          <!-- Date -->
+          <DateFormatter class="dateline" :data="project.data" />
+          <!-- Project Type -->
+          <span class="type">
+            {{ project.data.type }}
+          </span>
+          <!-- View Project -->
+          <nuxt-link class="view-project" :to="link(project)">
+            <span>View</span>
+            <img src="~/assets/img/smt-arrow.png" alt="">
+          </nuxt-link>
+        </div>
       </div>
-
-      <DateFormatter :data="project.data" />
     </div>
+    <!-- FAKE LINK - REPLACE WITH ACTUAL LINK -->
+    <a href="#" class="view-category">
+      All Projects
+      <img src="~/assets/img/smt-arrow.png" alt="">
+    </a>
   </div>
 </template>
 
 <script>
+import LinkResolver from "~/plugins/link-resolver.js"
 import textBalancer from 'text-balancer'
 import FormattedImage from '~/components/FormattedImage'
 import DateFormatter from '~/components/DateFormatter'
@@ -35,6 +50,11 @@ export default {
   },
   mounted() {
     textBalancer.balanceText()
+  },
+  methods: {
+    link(project) {
+      return LinkResolver(project)
+    }
   }
 }
 </script>
