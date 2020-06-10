e989e8fe90a68ba401dc1d1bd218ff491f36a843
StefKors <stef.kors@gmail.com>
2020-06-11 00:42:02 +0200

    add big project description content

--- components/BigProjectList.vue
+++ components/BigProjectList.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="project-wrapper large">
     <div class="block-project" v-for="project in projects" :key="project.id">
-      {{ getDescription(project) }}
       <FormattedImage
         class="project-img"
         :field="project.data.header"
@@ -16,33 +15,16 @@
             <!-- Date -->
             <DateFormatter class="dateline" :data="project.data" />
             <!-- Project Type -->
-            <span class="type">
-              {{ project.data.type }}
-            </span>
+            <span class="type">{{ project.data.type }}</span>
             <!-- View Project -->
             <nuxt-link class="view-project" :to="link(project)">
               <span>View</span>
-              <img src="~/assets/img/smt-arrow.png" alt="" />
+              <img src="~/assets/img/smt-arrow.png" alt />
             </nuxt-link>
           </div>
           <!-- THIS IS FAKE TEXT - NEEDS ACTUAL DATA -->
-          <div class="project-description">
-            <p>
-              For the last 32 years the north of The Netherlands has experienced
-              man-made earthquakes due to gas extraction in the largest field
-              for natural gas in Europe, the Groningen Gas Field. The work The
-              Intimate Earthquake Archive, brings together research done by
-              artist Sissel Marie Tonn with two different modes of ‘storing’
-              information of man-made earthquakes: The seismic activity of the
-              earthquakes as meticulously recorded and annotated in the immense
-              digital data bank of the Dutch Meteorological Institute (KNMI);
-              and the personal stories of people living in Groningen, who can
-              provide precise descriptions of feeling this seismic activity
-              passing through their bodies to anyone willing to listen. Some of
-              the people living with these man-made earthquakes even claim to
-              wake up in their beds, before they feel the tremors across the
-              earth.
-            </p>
+          <div v-if="getDescription(project)" class="project-description">
+            {{ getDescription(project) }}
           </div>
         </div>
       </div>
@@ -53,9 +35,11 @@
 <script>
 import LinkResolver from '~/plugins/link-resolver.js'
 import textBalancer from 'text-balancer'
+import get from 'lodash.get'
+import find from 'lodash.find'
+import stripHtml from 'string-strip-html'
 import FormattedImage from '~/components/FormattedImage'
 import DateFormatter from '~/components/DateFormatter'
-import get from 'lodash.get'
 
 export default {
   data() {
@@ -89,7 +73,25 @@ export default {
       return LinkResolver(project)
     },
     getDescription(project) {
-      return get(project, 'data')
+      // Get both description and reseach content
+      const description = get(project, ['data', 'description'])
+      const research = get(project, ['data', 'research'])
+      // set text as either description or research
+      const text = this.getText(description) || this.getText(research)
+
+      // if we have text
+      if (text) {
+        // render prismic slice as html
+        // then strip html so we have a flat paragraph
+        return stripHtml(this.$prismic.asHtml(text.primary.text))
+      }
+
+      // if we can't find any test return false
+      // so we don't render the whole div
+      return false
+    },
+    getText(slices) {
+      return find(slices, slice => slice.slice_type === 'text')
     }
   }
 }
