ce30843a25195037c584c8fa0387978604a9ae60
StefKors <stef.kors@gmail.com>
2020-06-20 14:55:14 +0200

    add footer colofon

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,19 +2,21 @@
   <div>
     <Header />
     <nuxt />
+    <Footer />
   </div>
 </template>
 <script>
 import Header from '~/components/Header'
+import Footer from '~/components/Footer'
 
 export default {
   components: {
-    Header
-  }
+    Header,
+    Footer,
+  },
 }
 </script>
 <style>
-
 html {
   scroll-behavior: smooth;
 }
