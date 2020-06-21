1075364cdec68bc3a8c752bf38527fec3fb486fb
Titus Knegtel <info@titusknegtel.nl>
2020-06-21 13:39:27 +0200

    Updated footer styling

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,10 +1,7 @@
 <template>
   <footer>
-    Design by
-    <nuxt-link to="http://titusknegtel.nl/" target="_blank"
-      >Titus Knegtel</nuxt-link
-    >, Development by
-    <nuxt-link to="http://stefkors.com/" target="_blank">Stef Kors</nuxt-link>
+    <p>Design by <nuxt-link to="http://titusknegtel.nl/" target="_blank">Titus Knegtel</nuxt-link></p>
+    <p>Development by <nuxt-link to="http://stefkors.com/" target="_blank">Stef Kors</nuxt-link></p>
   </footer>
 </template>
 <style lang="scss">
