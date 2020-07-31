f22cd4022740c6a8f8ea91ef0199855a44dc5dd4
StefKors <stef.kors@gmail.com>
2020-07-31 12:49:19 +0200

    WIP image placeholder loader
    Change-type: patch
    Signed-off-by: Stef Kors <stef@balena.io>

--- components/FormattedImage.vue
+++ components/FormattedImage.vue
@@ -1,10 +1,22 @@
 <template>
-  <img
-    loading="lazy"
-    v-if="Object.keys(field).length !== 0"
-    :srcset="field.url | params(this.width, this.height)"
-    :alt="field.alt"
-  />
+  <div class="FormattedImageWrapper">
+    <img
+      v-if="Object.keys(field).length !== 0"
+      :src="formatUrl(field.url, ...reduceFraction([this.width, this.height]))"
+      :alt="field.alt"
+      v-on:load="showFullImage"
+    />
+    <transition name="fade">
+      <img
+        loading="lazy"
+        v-if="Object.keys(field).length !== 0 && fullImage"
+        :srcset="params(field.url, this.width, this.height)"
+        :alt="field.alt"
+        class="placeholder"
+
+      />
+    </transition>
+  </div>
 </template>
 
 <script>
@@ -12,25 +24,68 @@ export default {
   props: {
     field: {
       type: Object,
-      required: true
+      required: true,
     },
     width: {
       type: Number,
-      required: false
+      required: false,
     },
     height: {
       type: Number,
-      required: false
+      required: false,
+    },
+  },
+  data() {
+    return {
+      fullImage: false,
     }
   },
-  filters: {
-    // https://user-guides.prismic.io/en/articles/3309829-image-optimization-imgix-integration
-    params(url, width = 'auto', height = 'auto') {
-      const size1 = `${url},fit=crop&auto=format&w=${width}&h=${height}`
-      const size2 = `${url},fit=crop&auto=format&w=${width*1.5}&h=${height*1.5} 1.5x`
-      const size3 = `${url},fit=crop&auto=format&w=${width*2}&h=${height*2} 2x`      
+  methods: {
+    reduceFraction(data) {
+      let numOne = Number(data[0])
+      let numTwo = Number(data[1])
+      let scaler = 2
+      for (var i = Math.max(numOne, numTwo); i > 1; i--) {
+        if (numOne % i == 0 && numTwo % i == 0) {
+          numOne /= i
+          numTwo /= i
+        }
+      }
+
+      return [numOne * scaler, numTwo * scaler]
+    },
+    formatUrl(url, width = 'auto', height = 'auto', scale = 1) {
+      return `${url},fit=crop&w=${width * scale}&h=${height * scale} ${scale}x`
+    },
+    showFullImage() {
+      this.fullImage = true
+    },
+    params(url, width, height) {
+      const size1 = this.formatUrl(url, width, height, 1)
+      const size2 = this.formatUrl(url, width, height, 1.5)
+      const size3 = this.formatUrl(url, width, height, 2)
       return `${size1}, ${size2}, ${size3}`
-    }
+    },
   },
 }
 </script>
+<style lang="scss">
+.fade-enter-active {
+  transition: opacity 0.1s;
+}
+.fade-enter {
+  opacity: 0;
+}
+
+.FormattedImageWrapper {
+  position: relative;
+}
+
+.placeholder {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
+</style>
