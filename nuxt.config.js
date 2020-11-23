19b9e64efe6d34346cc21f441e23654f7c58dcd0
StefKors <stef.kors@gmail.com>
2020-11-23 21:21:16 +0100

    Set SSR false
    Change-type: minor
    Signed-off-by: Stef Kors <stef@balena.io>

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,5 @@
 export default {
-  /*
-  ** Nuxt rendering mode
-  ** See https://nuxtjs.org/api/configuration-mode
-  */
- mode: 'universal',
+  ssr: false,
  /*
  ** Nuxt target
  ** See https://nuxtjs.org/api/configuration-target
